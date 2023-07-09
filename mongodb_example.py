b76e0f62fee784aa1f19c2b8d21fa29bb37a83c5
BossBaby25 <108459353+BossBaby25@users.noreply.github.com>
2023-07-10 01:13:10 +0600

    Update mongodb_example.py

--- mongodb_example.py
+++ mongodb_example.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 from pymongo import MongoClient
 import serial
+import Adafruit_DHT
 
 # MongoDB connection settings
 mongodb_uri = "mongodb+srv://meraj154213:iCFmmhPjFdUk2hvV@cluster0.hj5abn5.mongodb.net/?retryWrites=true&w=majority"
@@ -12,22 +13,30 @@ GPIO.setmode(GPIO.BCM)
 # Serial port settings for Arduino
 serial_port = serial.Serial('/dev/ttyACM0', baudrate=4800, timeout=1)
 
+# DHT22 sensor pin
+dht_pin = 4
+
 def read_npk_sensor():
     # Read the response from Arduino
     arduino_data = serial_port.readline().decode().strip()
     npk_data = arduino_data.split(',')
-    
+
     print("Received data from Arduino:", npk_data)  # Debugging line
-    
-    if len(npk_data) >= 3:
+
+    if len(npk_data) >= 4:
         nitrogen = npk_data[0]
         phosphorus = npk_data[1]
         potassium = npk_data[2]
         soil_moisture = npk_data[3]
+        humidity, temperature = read_dht_sensor()
     else:
-        nitrogen, phosphorus, potassium, soil_moisture = "N/A", "N/A", "N/A", "N/A"
+        nitrogen, phosphorus, potassium, soil_moisture, humidity, temperature = "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
 
-    return nitrogen, phosphorus, potassium, soil_moisture
+    return nitrogen, phosphorus, potassium, soil_moisture, humidity, temperature
+
+def read_dht_sensor():
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, dht_pin)
+    return humidity, temperature
 
 try:
     # Connect to MongoDB
@@ -38,15 +47,18 @@ try:
     collection = db['test11db']
 
     while True:
-        nitrogen, phosphorus, potassium, soil_moisture = read_npk_sensor()
-        print("NPK levels - N: {}, P: {}, K: {},, soil_moisture: {}".format(nitrogen, phosphorus, potassium, soil_moisture))
+        nitrogen, phosphorus, potassium, soil_moisture, humidity, temperature = read_npk_sensor()
+        print("NPK levels - N: {}, P: {}, K: {}, soil_moisture: {}".format(nitrogen, phosphorus, potassium, soil_moisture))
+        print("DHT22 sensor - Humidity: {:.2f}%, Temperature: {:.2f}°C".format(humidity, temperature))
 
         # Prepare the document to be inserted into the collection
         document = {
             'nitrogen': nitrogen,
             'phosphorus': phosphorus,
             'potassium': potassium,
-            'soil_moisture' : soil_moisture,
+            'soil_moisture': soil_moisture,
+            'humidity': humidity,
+            'temperature': temperature,
             'timestamp': time.time()
         }
 
